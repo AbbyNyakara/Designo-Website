8a3eb9f95a1584bd9a26741cf5133b18a8c307bd
Abby Nyakara <abbynyakara@gmail.com>
2023-01-02 16:50:04 +0300

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import './footer.scss';
 import logo from '../../assets/logo-dark.png';
 import Header from '../Header/Header';
+import facebookIcon from '../../assets/icon-facebook.svg';
+import youTubeIcon from '../../assets/icon-youtube.svg';
+import twitterIcon from '../../assets/icon-twitter.svg';
+import pinterestIcon from '../../assets/icon-pinterest.svg';
+import instagramIcon from '../../assets/icon-instagram.svg';
 
 const Footer = () => {
   return (
@@ -18,6 +23,14 @@ const Footer = () => {
           Contact Us (Central Office) <br />
           P :+1 253-863-8967 <br />
           M : contact@designo.com
+        </div>
+
+        <div className="footer__socials">
+          <img src={facebookIcon} alt="" />
+          <img src={youTubeIcon} alt="" />
+          <img src={twitterIcon} alt="" />
+          <img src={pinterestIcon} alt="" />
+          <img src={instagramIcon} alt="" /> 
         </div>
       </div>
     </div>
