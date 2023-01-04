eea3bfa5b218af6e08fde78d962da9f29ed27126
Abby Nyakara <abbynyakara@gmail.com>
2023-01-04 21:15:47 +0300

    Refactor props

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -6,11 +6,14 @@ import resourcefulImg from '../../assets/illustration-resourceful.svg';
 import friendlyImg from '../../assets/illustration-friendly.svg';
 
 const AboutCard = (props) => {
+  
+  const { image, title, description} = props
+
   return (
     <div className='about__card'> 
-      <img src={props.image} alt="" className='card__image'/>
-      <h3 className='card__title'>{props.title}</h3>
-      <p className='card__description'>{props.description}</p>
+      <img src={image} alt="" className='card__image'/>
+      <h3 className='card__title'>{title}</h3>
+      <p className='card__description'>{description}</p>
     </div>
   )
 };
--- src/components/CompanyCard/CompanyCard.jsx
+++ src/components/CompanyCard/CompanyCard.jsx
@@ -12,17 +12,19 @@ const CompanyCard = (props) => {
     color: '#e88069'
   }
 
+  const { title, description, style, image} = props
+
   return (
-    <div className='company__card' style={props.style}>
+    <div className='company__card' style={style}>
       <div className="company__card-left">
-        <h2 className='company__card-title'>{props.title}</h2>
+        <h2 className='company__card-title'>{title}</h2>
         <p className='company__card-description'>
-          {props.description}
+          {description}
         </p>
       </div>
 
       <div className="company__card-right">
-        <img src={props.image} alt="" className='company__card-image' />
+        <img src={image} alt="" className='company__card-image' />
       </div>
     </div>
   )
@@ -32,7 +34,7 @@ CompanyCard.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   style: PropTypes.element,
-  image: stylePropType
+  image: PropTypes.stylePropType
 }
 
 
