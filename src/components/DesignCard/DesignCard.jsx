02b34c8b1844cd8202652b4ac72e64d925e2685f
Abby Nyakara <abbynyakara@gmail.com>
2023-01-05 16:13:33 +0300

    create web design page

--- src/components/DesignCard/DesignCard.jsx
+++ src/components/DesignCard/DesignCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import './designCard.scss';
-import imageBuilderImg from '../../assets/image-builder.jpg';
 
-const DesignCard = () => {
+
+const DesignCard = (props) => {
+
+  const {image, title, description} = props;
   return (
     <div className='design__card'>
       <div className="design__card-img">
-        <img src={imageBuilderImg} alt="" />
+        <img src={image} alt="" />
       </div>
       <div className="design__card-main">
-        <h3 className='design__card-title'>Builder</h3>
-        <p className='design__card-description'>Connects users with local contractors based on their location</p>
+        <h3 className='design__card-title'>{title}</h3>
+        <p className='design__card-description'>{description}</p>
       </div>
     </div>
   )
